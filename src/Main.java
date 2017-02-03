986b5a91e62c967da1cf98598a2232fbbeb5c059
radeshshiwlochan <radesh0430@gmail.com>
2017-02-03 00:00:53 -0500

    method writes input to an output file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main  {
 	private int [] histogram;
@@ -9,10 +10,11 @@ public class Main  {
 	private int minVal;
 	private int maxVal;
 	
-	public Main(String inputFile)  {
+	public Main(String inputFile, String outputFile)  {
 		int num, count = 0;
 		try {
 			Scanner readFile = new Scanner(new File(inputFile));
+			PrintWriter writeToFile = new PrintWriter(new File(outputFile));
 			numRows = readFile.nextInt();
 			numCols = readFile.nextInt();
 			minVal = readFile.nextInt();
@@ -21,6 +23,7 @@ public class Main  {
 			while(readFile.hasNextInt()) {
 				num = readFile.nextInt();
 				System.out.print(num + " ");
+				writeToFile.print("" + String.format("%2d", num) + "\t");
 				count++;
 				if(count == numCols) {
 					System.out.println();
@@ -28,20 +31,23 @@ public class Main  {
 				}
 					
 			}
+			writeToFile.flush();
 		} catch(IOException exc) {
 			System.out.println("Error occurred reading in file " + exc);
 		}
 	}
 
 	public static void main(String[] args) {
-		if(args.length != 1) {
+		if(args.length != 2) {
 			System.out.println("Needs an input file");
 			System.exit(-1);
 		}
 		
 		try {
 		String inputFile = args[0];	
-			Main mainObj = new Main(inputFile);
+		String outputFile = args[1];
+		
+			Main mainObj = new Main(inputFile, outputFile);
 		} catch (Exception ioe) {
 			System.out.println(ioe);
 		}
